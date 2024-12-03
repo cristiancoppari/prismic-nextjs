5e5b329ee290cec4d242e35a86afc63083639080
Cristian <cristian.coppari@gmail.com>
2024-12-03 19:57:34 -0300

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <main>Home Page</main>;
+import { Metadata } from "next";
+// import { SliceZone } from "@prismicio/react";
+
+import { createClient } from "@/prismicio";
+// import { components } from "@/slices";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("home_page");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
+}
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("home_page");
+  console.log(page);
+
+  return <div>It works!</div>;
+
+  // return <SliceZone slices={page.data.slices} components={components} />;
 }
